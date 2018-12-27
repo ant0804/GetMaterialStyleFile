8fad0f20091dcff7b8897d58f5e853c2c3616bff
John <segma0804@gmail.com>
2018-12-27 17:06:47 +0800

    Initial commit

---
+++ MaterialStyleContentFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GetMaterialStyleFile
+{
+    class MaterialStyleContentFile
+    {
+
+        [JsonProperty("html")]
+        public List<string> Html { get; set; }
+
+        [JsonProperty("css")]
+        public List<string> Css { get; set; }
+
+        [JsonProperty("js")]
+        public List<string> Js { get; set; }
+
+        [JsonProperty("colorTheme")]
+        public List<string> ColorTheme { get; set; }
+
+        [JsonProperty("colorWeight")]
+        public List<string> ColorWeight { get; set; }
+
+        [JsonProperty("img")]
+        public List<string> Img { get; set; }
+    }
+}
